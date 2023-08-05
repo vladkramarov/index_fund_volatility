277ff91b321430f51aaee18f01dbab99ac0b9608
vladkramarov <kramaroff01@gmail.com>
2023-08-05 18:17:01 -0500

    changed model path

--- core.py
+++ core.py
@@ -25,7 +25,8 @@ FORECAST_HORIZONS = ['one_days', 'two_days', 'three_days', 'four_days', 'five_da
 ADDITIONAL_OUTPUT_COLS = ['volatility_target', 'date', 'ticker', 'idx']
 
 LIGHTNING_LOGS_DIR = ROOT_FOLDER / 'lightning_logs'
-BEST_MODEL_PATH = ROOT_FOLDER /  'version_175/checkpoints/epoch=21-step=9526.ckpt'
+BEST_MODEL_PATH = ROOT_FOLDER /  'model_path/checkpoints/epoch=9-step=4330.ckpt'
+# 'version_175/checkpoints/epoch=21-step=9526.ckpt'
 #LIGHNING_LOGS_DIR / 'lightning_logs/version_120/checkpoints/epoch=65-step=30426.ckpt'
 TIMESERIES_DATASET_PARAMS = ROOT_FOLDER / 'ts_dataset_params.joblib'
 
