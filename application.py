15de4cd7013e90b05f7257ce1d77ceb5cbbe44c7
vladkramarov <kramaroff01@gmail.com>
2023-08-08 11:28:55 -0500

    ...

--- application.py
+++ application.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, HTTPException
 import uvicorn
+import deployment.predict
 import data_processing.input_validation as input_validation
 from typing import Dict, List
 import data_processing.new_data_processor as new_data_processor
@@ -9,6 +10,7 @@ import pandas as pd
 import logging
 from pytorch_forecasting import TemporalFusionTransformer, TimeSeriesDataSet
 import time
+import json
 import data_processing.output_formatter as output_formatter
 
 logging.basicConfig(level=logging.INFO)
@@ -20,49 +22,36 @@ application = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-# @application.post("/predict")
-# async def predict(input_data: Dict):
-#     input_data, errors = input_validation.validate_inputs(input_data)
-#     results = {}
-#     if errors is not None:
-#         raise HTTPException(status_code=400, detail=errors)
-#     else:
-#         processed_data, _ = new_data_processor.new_data_pipeline(tickers = input_data['tickers'], prediction_start_date = input_data['prediction_start_date'])
-#         logger.info(f'Processed data: {processed_data.head()}')
-#         model = loader.get_model()
-#         ts_dataset_params = loader.get_timeseries_params()
-#         ts_dataset = TimeSeriesDataSet.from_parameters(ts_dataset_params, processed_data, predict=False)
-#         start_time = time.time()
-#         preds = None
-#         try:
-#             preds = model.predict(ts_dataset, return_index=True, return_x=True, mode='quantiles')
-#         except Exception as e:
-#             logger.exception("Error during prediction")
-
-#         end_time = time.time()
-#         output = preds[-1].replace(np.nan, "N/A", inplace=True).to_dict()
-#         logger.info(f"Model prediction took {end_time - start_time} seconds")
-#         # processed_output = deployment.predict.process_output(preds, input_data['prediction_start_date'], processed_data)
-#         # processed_output.replace(np.nan, "N/A", inplace=True)
-#         results['results'] = output
-    
-#     results['errors'] = errors
-#     return results
-
-@application.post("/predict_new")
-async def predict_new(input_data: Dict):
-    processed_data = pd.DataFrame(input_data['processed_data'])
-    model = loader.get_model()
-    ts_dataset_params = loader.get_timeseries_params()
-    ts_dataset = TimeSeriesDataSet.from_parameters(ts_dataset_params, processed_data, predict=False)
+@application.post("/predict")
+async def predict(input_data: Dict):
+    input_data, errors = input_validation.validate_inputs(input_data)
     results = {}
-    preds = model.predict(ts_dataset, return_index=True, return_x=True, mode='quantiles')
-    index = preds.index
-    output = preds[0].to('cpu').detach().numpy()
-    logger.info(f'Index shape: {index.shape}')
-    logger.info(f'Index type: {type(index)}')
-    formatter = output_formatter.OutputFormatter(earliest_prediction_date='2023-07-25', prediction=preds)
-    final = formatter.get_unaligned_results(processed_data)
-    final.replace(np.nan, "N/A", inplace=True)
-    results['results'] = final.to_dict(orient='records')
+    if errors is not None:
+        raise HTTPException(status_code=400, detail=json.loads([errors]))
+    else:
+        processed_data, _ = new_data_processor.new_data_pipeline(tickers = input_data['tickers'], prediction_start_date = input_data['prediction_start_date'])
+        preds = deployment.predict.predict(processed_data)
+        processed_output = deployment.predict.process_output(preds, input_data['prediction_start_date'], processed_data)
+        processed_output.replace(np.nan, "N/A", inplace=True)
+        results['results'] = processed_output.to_dict(orient='records')
+    
+    results['errors'] = errors
     return results
+
+# @application.post("/predict_new")
+# async def predict_new(input_data: Dict):
+#     processed_data = pd.DataFrame(input_data['processed_data'])
+#     model = loader.get_model()
+#     ts_dataset_params = loader.get_timeseries_params()
+#     ts_dataset = TimeSeriesDataSet.from_parameters(ts_dataset_params, processed_data, predict=False)
+#     results = {}
+#     preds = model.predict(ts_dataset, return_index=True, return_x=True, mode='quantiles')
+#     index = preds.index
+#     output = preds[0].to('cpu').detach().numpy()
+#     logger.info(f'Index shape: {index.shape}')
+#     logger.info(f'Index type: {type(index)}')
+#     formatter = output_formatter.OutputFormatter(earliest_prediction_date='2023-07-25', prediction=preds)
+#     final = formatter.get_unaligned_results(processed_data)
+#     final.replace(np.nan, "N/A", inplace=True)
+#     results['results'] = final.to_dict(orient='records')
+#     return results
--- deployment/predict.py
+++ deployment/predict.py
@@ -9,11 +9,12 @@ import data_processing.output_formatter as output_formatter
 importlib.reload(core)
 
 def predict(processed_data: pd.DataFrame):
+    processed_data['date'] = processed_data['date'].astype(str)
     model = loader.get_model()
     ts_dataset_params = loader.get_timeseries_params()
     ts_dataset = TimeSeriesDataSet.from_parameters(ts_dataset_params, processed_data, predict=False)
     preds = model.predict(ts_dataset, return_index=True, return_x=True, mode='quantiles')
-    return preds, ts_dataset
+    return preds
 
 def process_output(preds, prediction_start_date: str, processed_data: pd.DataFrame) -> pd.DataFrame:
     output_form = output_formatter.OutputFormatter(earliest_prediction_date=prediction_start_date, prediction=preds)
