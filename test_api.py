ea209a6a5276b01566002117e2f4c5cdcaf8ba35
vladkramarov <kramaroff01@gmail.com>
2023-08-08 07:34:31 -0500

    ...

--- test_api.py
+++ test_api.py
@@ -7,3 +7,4 @@ local_host = 'http://pytorch-final-env.eba-zbppsjpu.us-east-1.elasticbeanstalk.c
 input_data = {'tickers': ['AAPL'], 'prediction_start_date': '2023-08-07'}
 
 results = requests.post(local_host + '/predict', json=input_data)
+
