dc30f661abaff8679f6f8a8bad09cc1933b13b39
vladkramarov <kramaroff01@gmail.com>
2023-08-08 08:24:02 -0500

    ...

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, HTTPException
-import deployment.predict 
 import uvicorn
 import data_processing.input_validation as input_validation
 from typing import Dict, List
@@ -8,6 +7,7 @@ import loader
 import numpy as np
 import logging
 from pytorch_forecasting import TemporalFusionTransformer, TimeSeriesDataSet
+import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -29,10 +29,15 @@ async def predict(input_data: Dict):
         model = loader.get_model()
         ts_dataset_params = loader.get_timeseries_params()
         ts_dataset = TimeSeriesDataSet.from_parameters(ts_dataset_params, processed_data, predict=False)
+        start_time = time.time()
         try:
             preds = model.predict(ts_dataset, return_index=True, return_x=True, mode='quantiles')
         except Exception as e:
             logger.exception("Error during prediction")
+
+        end_time = time.time()
+
+        logger.info(f"Model prediction took {end_time - start_time} seconds")
         # processed_output = deployment.predict.process_output(preds, input_data['prediction_start_date'], processed_data)
         # processed_output.replace(np.nan, "N/A", inplace=True)
         results['results'] = preds[-1].to_dict(orient='records')
